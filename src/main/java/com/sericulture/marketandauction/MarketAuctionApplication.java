975ead814203a3c5e4b995e145c549a94e75e538
jaibharata <145244644+jaibharata@users.noreply.github.com>
2023-11-01 09:59:08 +0530

    Initial commit

---
+++ src/main/java/com/sericulture/marketandauction/MarketAuctionApplication.java
@@ -0,0 +1,13 @@
+package com.sericulture.marketandauction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketAuctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketAuctionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sericulture/marketandauction/MarketAuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sericulture.marketandauction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketAuctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
