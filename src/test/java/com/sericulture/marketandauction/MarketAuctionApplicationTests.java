cdd1cd885963f348a49d557f6c336378ed0f54d7
jaibharata <145244644+jaibharata@users.noreply.github.com>
2023-11-12 19:46:37 +0530

    updates

--- src/test/java/com/sericulture/marketandauction/MarketAuctionApplicationTests.java
+++ src/test/java/com/sericulture/marketandauction/MarketAuctionApplicationTests.java
@@ -2,7 +2,7 @@ package com.sericulture.marketandauction;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+//Not required if test cases are not written
 //@SpringBootTest
 class MarketAuctionApplicationTests {
 
