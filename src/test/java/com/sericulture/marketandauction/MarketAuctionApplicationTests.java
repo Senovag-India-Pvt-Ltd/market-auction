f9070f61ac82726af42acdda98400345275717ef
jaibharata <145244644+jaibharata@users.noreply.github.com>
2023-11-12 19:27:19 +0530

    updates commenting surefire plugin

--- src/test/java/com/sericulture/marketandauction/MarketAuctionApplicationTests.java
+++ src/test/java/com/sericulture/marketandauction/MarketAuctionApplicationTests.java
@@ -3,7 +3,7 @@ package com.sericulture.marketandauction;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class MarketAuctionApplicationTests {
 
 	@Test
