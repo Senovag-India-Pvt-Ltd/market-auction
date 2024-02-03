17761171b48f18e633e40de3d42dcb02bc1c6d19
Mylari1644 <mylari1644@gmail.com>
2024-02-03 23:44:53 +0530

    defect fix

--- src/main/java/com/sericulture/marketandauction/helper/MarketAuctionQueryConstants.java
+++ src/main/java/com/sericulture/marketandauction/helper/MarketAuctionQueryConstants.java
@@ -14,7 +14,7 @@ public class MarketAuctionQueryConstants {
             SELECT MAX(AMOUNT) AMT, ALLOTTED_LOT_ID  from REELER_AUCTION ra
             where AUCTION_DATE = :today and ALLOTTED_LOT_ID in ( :lotList) AND MARKET_ID =:marketId GROUP by ALLOTTED_LOT_ID ) as RAB 
             WHERE RAB.AMT=RA.AMOUNT AND  RA.MARKET_ID =:marketId AND RA.ALLOTTED_LOT_ID = RAB.ALLOTTED_LOT_ID AND AUCTION_DATE = :today
-            GROUP by  RA.ALLOTTED_LOT_ID ) RA ON RA.ID= RAA.REELER_AUCTION_ID
+            GROUP by  RA.ALLOTTED_LOT_ID ) RA ON RA.ID= RAA.REELER_AUCTION_ID 
             """;
 
     public static final String ALLTTOTED_LOT_LIST_PER_MARKET_ID = """
@@ -23,19 +23,21 @@ public class MarketAuctionQueryConstants {
             on ma.market_auction_id = l.market_auction_id
             where ma.market_auction_date =:auctionDate and ma.market_id = :marketId  and l.status is NULL """;
 
-    public static final String ALLTTOTED_LOT_LIST_PER_MARKET_ID_AND_GODOWNID = ALLTTOTED_LOT_LIST_PER_MARKET_ID + " and ma.godown_id =:godownId";
+    public static final String ALLTTOTED_LOT_LIST_PER_MARKET_ID_AND_GODOWNID = ALLTTOTED_LOT_LIST_PER_MARKET_ID + " and ma.godown_id =:godownId ";
 
     private static final String SELECT_FIELDS_DTR_ONLINE = """
             select  ROW_NUMBER() OVER(ORDER BY l.lot_id ASC) AS row_id,l.allotted_lot_id ,f.first_name,f.middle_name,f.last_name,f.farmer_number,
             f.mobile_number,l.LOT_WEIGHT_AFTER_WEIGHMENT,ra.AMOUNT,l.LOT_SOLD_OUT_AMOUNT ,l.MARKET_FEE_FARMER,l.MARKET_FEE_REELER,
             r.reeling_license_number,r.name,r.mobile_number,
-            fba.farmer_bank_name,fba.farmer_bank_branch_name ,fba.farmer_bank_ifsc_code ,fba.farmer_bank_account_number""";
+            fba.farmer_bank_name,fba.farmer_bank_branch_name ,fba.farmer_bank_ifsc_code ,fba.farmer_bank_account_number """;
 
-    private static final String FROM =" from";
+    private static final String FROM =" from ";
+
+    private static final String SPACE = " ";
 
 
     private static final String LOT_ACCEPTED_ALL_TABLES_FROM_CLAUSE_FARMER = """
-            FARMER dbo.f
+             FARMER f
             INNER JOIN dbo.market_auction ma ON ma.farmer_id = f.FARMER_ID 
             INNER JOIN dbo.lot l ON l.market_auction_id =ma.market_auction_id and l.auction_date = ma.market_auction_date 
             INNER JOIN dbo.REELER_AUCTION ra ON ra.REELER_AUCTION_ID  = l.REELER_AUCTION_ID and ra.STATUS ='accepted' and ra.AUCTION_DATE =l.auction_date 
@@ -45,17 +47,17 @@ public class MarketAuctionQueryConstants {
             """;
 
     private static final String LOT_ACCEPTED_ALL_TABLES_FROM_CLAUSE_REELER = """
-             INNER JOIN dbo.reeler r ON r.reeler_id =ra.REELER_ID  
+              INNER JOIN dbo.reeler r ON r.reeler_id =ra.REELER_ID  
              LEFT JOIN dbo.reeler_virtual_bank_account rvba ON rvba.reeler_id =r.reeler_id and rvba.market_master_id = ma.market_id
-             LEFT JOIN dbo.REELER_VID_CURRENT_BALANCE rvcb ON rvcb.reeler_virtual_account_number= rvba.virtual_account_number""";
+             LEFT JOIN dbo.REELER_VID_CURRENT_BALANCE rvcb ON rvcb.reeler_virtual_account_number= rvba.virtual_account_number """;
 
     private static final String SELECT_FIELDS_FARMER_TXN = """
-            select  ROW_NUMBER() OVER(ORDER BY l.lot_id ASC) AS row_id,l.allotted_lot_id ,l.auction_date,
+             select  ROW_NUMBER() OVER(ORDER BY l.lot_id ASC) AS row_id,l.allotted_lot_id ,l.auction_date,
             f.first_name,f.middle_name,f.last_name,f.farmer_number,
-            l.LOT_WEIGHT_AFTER_WEIGHMENT,ra.AMOUNT,l.LOT_SOLD_OUT_AMOUNT ,l.MARKET_FEE_FARMER""";
+            l.LOT_WEIGHT_AFTER_WEIGHMENT,ra.AMOUNT,l.LOT_SOLD_OUT_AMOUNT ,l.MARKET_FEE_FARMER """;
 
     private static final String WHERE_CLAUSE_DTR_ONLINE = """
-            where l.status in ('readyforpayment','paymentsuccess','paymentfailed','paymentprocessing')
+              where l.status in ('readyforpayment','paymentsuccess','paymentfailed','paymentprocessing')
              and l.auction_date BETWEEN :fromDate and :toDate 
              and l.market_id =:marketId 
              and (:reelerIdList is null OR r.reeler_id in (:reelerIdList))
@@ -82,14 +84,14 @@ public class MarketAuctionQueryConstants {
             ORDER by l.lot_id""";
 
     private static final String WHERE_CLAUSE_AUCTION_DATE_LIST = """
-             where l.status =:lotStatus
+                 where l.status =:lotStatus
                          and l.market_id =:marketId
                          and fba.farmer_bank_account_number != ''
                          and fba.farmer_bank_ifsc_code !=''
                          and rvcb.CURRENT_BALANCE > 0.0
                         ORDER by l.auction_date""";
 
-    public static final String AUCTION_DATE_LIST_BY_LOT_STATUS = "select  distinct l.auction_date" + FROM + LOT_ACCEPTED_ALL_TABLES_FROM_CLAUSE_FARMER +LOT_ACCEPTED_ALL_TABLES_FROM_CLAUSE_REELER+ WHERE_CLAUSE_AUCTION_DATE_LIST;
+    public static final String AUCTION_DATE_LIST_BY_LOT_STATUS = "select  distinct l.auction_date " + FROM + SPACE + LOT_ACCEPTED_ALL_TABLES_FROM_CLAUSE_FARMER + SPACE  +LOT_ACCEPTED_ALL_TABLES_FROM_CLAUSE_REELER+ SPACE + WHERE_CLAUSE_AUCTION_DATE_LIST;
 
     public static final String NEWLY_CREATED_LOTS = """
             select  f.farmer_number,f.first_name ,f.middle_name,
@@ -151,9 +153,9 @@ public class MarketAuctionQueryConstants {
 
     public static final String PENDING_REPORT_NEWLY_CREATED_LOTS = NEWLY_CREATED_LOTS + " and (:lotList is null OR l.allotted_lot_id not in (:lotList))";
 
-    public static final String PRINT_REPORT_NEWLY_CREATED_LOT_ID = NEWLY_CREATED_LOTS + AND_LOT_ID + ACTIVE_FILTERS_NEWLY_CREATED;
+    public static final String PRINT_REPORT_NEWLY_CREATED_LOT_ID = NEWLY_CREATED_LOTS + SPACE +AND_LOT_ID +SPACE+ ACTIVE_FILTERS_NEWLY_CREATED;
 
-    public static final String PRINT_REPORT_ACCEPTED_LOT_ID = ACCEPTED_LOTS + AND_LOT_ID + ACTIVE_FILTERS_NEWLY_CREATED + ACTIVE_FILTERS_ACCEPTED_CREATED;
+    public static final String PRINT_REPORT_ACCEPTED_LOT_ID = ACCEPTED_LOTS + AND_LOT_ID + ACTIVE_FILTERS_NEWLY_CREATED+SPACE + ACTIVE_FILTERS_ACCEPTED_CREATED;
 
     public static final String LOT_CLAUSE_FOR_PENDING_REPORT =  " and ( l.status IS NULL OR l.status='accepted')";
 
