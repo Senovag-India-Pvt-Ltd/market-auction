9d51e0a43abe6cff21c4b84d8febd2bbe6cff246
swathivs810 <152892551+swathivs810@users.noreply.github.com>
2024-07-27 15:36:48 +0530

    status for bid and acceptance through stored procedure

--- src/main/java/com/sericulture/marketandauction/service/MarketAuctionReportService.java
+++ src/main/java/com/sericulture/marketandauction/service/MarketAuctionReportService.java
@@ -1055,7 +1055,7 @@ public static Form13TotalResponse calculateTotalValues(List<GroupLotStatus> tota
         }
         return groupLotList;
     }
-    public ResponseEntity<?> getDashboardReport(DashboardReportRequest dashboardReportRequest) {
+    /*public ResponseEntity<?> getDashboardReport(DashboardReportRequest dashboardReportRequest) {
 
         ResponseWrapper rw = ResponseWrapper.createWrapper(List.class);
 
@@ -1110,6 +1110,93 @@ public static Form13TotalResponse calculateTotalValues(List<GroupLotStatus> tota
         rw.setContent(dashboardReport);
         return ResponseEntity.ok(rw);
 
+    }*/
+
+    public ResponseEntity<?> getDashboardReport(DashboardReportRequest dashboardReportRequest) {
+
+        ResponseWrapper rw = ResponseWrapper.createWrapper(List.class);
+
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSSSSS");
+        String formattedDateTime = now.format(formatter);
+
+        EntityManager entityManager = null;
+        EntityManager entityManager2 = null;
+        try {
+            entityManager = entityManagerFactory.createEntityManager();
+            StoredProcedureQuery procedureQuery = entityManager
+                    .createStoredProcedureQuery("sp_CheckBidStatus");
+
+            procedureQuery.registerStoredProcedureParameter("marketId", Integer.class, ParameterMode.IN);
+
+            entityManager.getTransaction().begin();
+            procedureQuery.setParameter("marketId", dashboardReportRequest.getMarketId());
+            procedureQuery.execute();
+
+            entityManager.getTransaction().commit();
+            List<Object[]> responsesIsAuctionStarted = procedureQuery.getResultList();
+
+            entityManager2 = entityManagerFactory.createEntityManager();
+            StoredProcedureQuery procedureQuery2 = entityManager
+                    .createStoredProcedureQuery("sp_CheckAuctionStatus");
+
+            procedureQuery2.registerStoredProcedureParameter("marketId", Integer.class, ParameterMode.IN);
+
+            entityManager2.getTransaction().begin();
+            procedureQuery2.setParameter("marketId", dashboardReportRequest.getMarketId());
+            procedureQuery2.execute();
+
+            entityManager2.getTransaction().commit();
+            List<Object[]> responsesIsAcceptanceStarted = procedureQuery2.getResultList();
+
+            // List<Object[]> responsesIsAuctionStarted = lotRepository.getIsAuctionStarted(dashboardReportRequest.getMarketId(), formattedDateTime);
+            //List<Object[]> responsesIsAcceptanceStarted = lotRepository.getIsAcceptanceStarted(dashboardReportRequest.getMarketId(), formattedDateTime);
+            List<Object[]> marketNameResponse = lotRepository.getMarketName(dashboardReportRequest.getMarketId());
+            List<Object[]> responses = lotRepository.getDashboardCount(dashboardReportRequest.getMarketId(), dashboardReportRequest.getDashboardReportDate());
+
+            if (Util.isNullOrEmptyList(responses)) {
+                throw new ValidationException("No data found");
+            }
+
+            DashboardReport dashboardReport = new DashboardReport();
+            dashboardReport.setMarketName(Util.objectToString(marketNameResponse.get(0)[0]));
+            dashboardReport.setAuctionStarted(Util.objectToString(responsesIsAuctionStarted.get(0)[0]));
+            dashboardReport.setAcceptanceStarted(Util.objectToString(responsesIsAcceptanceStarted.get(0)[0]));
+
+            List<DashboardReportInfo> dashboardReportInfoList = new ArrayList<>();
+            for (Object[] response : responses) {
+                DashboardReportInfo dashboardReportInfo = DashboardReportInfo.builder()
+                        .raceName(Util.objectToString(response[0]))
+                        .totalLots(Util.objectToString(response[1]))
+                        .totalSoldOutAmount(Util.objectToString(response[2]))
+                        .totalLotsBid(Util.objectToString(response[3]))
+                        .totalBids(Util.objectToString(response[4]))
+                        .totalReelers(Util.objectToString(response[5]))
+                        .accecptedLots(Util.objectToString(response[6]))
+                        .accecptedLotsMaxBid(Util.objectToString(response[7]))
+                        .accectedLotsMinBid(Util.objectToString(response[8]))
+                        .averagRate(Util.objectToString(response[9]))
+                        .weighedLots(Util.objectToString(response[10]))
+                        .totalLotsBid(Util.objectToString(response[11]))
+                        .currentAuctionMaxAmount(Util.objectToString(response[12]))
+                        .totalLotsNotBid(Util.objectToString(response[13]))
+                        .build();
+
+                dashboardReportInfoList.add(dashboardReportInfo);
+            }
+            dashboardReport.setDashboardReportInfoList(dashboardReportInfoList);
+            rw.setContent(dashboardReport);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            if (entityManager != null && entityManager.isOpen()) {
+                entityManager.close();
+            }
+            if (entityManager2 != null && entityManager2.isOpen()) {
+                entityManager2.close();
+            }
+        }
+        return ResponseEntity.ok(rw);
     }
 
     public ResponseEntity<?> getAverageReportForYearsReport(AverageReportRequest averageReportRequest) {
