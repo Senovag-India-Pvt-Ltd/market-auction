9ec795e24170cf0162d1a95f917ce3e6bb2e5d97
Shraddha <shraddhanku2001@gmail.com>
2024-10-11 13:44:51 +0530

    Save Pupa Test details

--- src/main/java/com/sericulture/marketandauction/controller/CocoonMarketController.java
+++ src/main/java/com/sericulture/marketandauction/controller/CocoonMarketController.java
@@ -1,7 +1,11 @@
 package com.sericulture.marketandauction.controller;
 
+import com.sericulture.marketandauction.model.ResponseWrapper;
 import com.sericulture.marketandauction.model.api.cocoon.LotBasePriceFixationRequest;
+import com.sericulture.marketandauction.model.api.cocoon.PupaTestAndCocoonAssessmentRequest;
 import com.sericulture.marketandauction.model.api.cocoon.PupaTestResultFinderRequest;
+import com.sericulture.marketandauction.model.api.marketauction.LotGroupageDetailsRequest;
+import com.sericulture.marketandauction.model.api.marketauction.LotGroupageResponse;
 import com.sericulture.marketandauction.model.api.marketauction.MarketAuctionRequest;
 import com.sericulture.marketandauction.model.entity.LotBasePriceFixation;
 import com.sericulture.marketandauction.service.CocoonMarketService;
@@ -30,6 +34,15 @@ public class CocoonMarketController {
     @GetMapping("/getLast10daysBasePrice")
     public ResponseEntity<?> getLast10daysBasePrice(){
         return cocoonMarketService.getLast10DaysPrices();
+    }
+
+    @PostMapping("/savePupaTestAndCocoonAssessmentResult")
+    public ResponseEntity<?> savePupaTestAndCocoonAssessmentResult(@RequestBody PupaTestAndCocoonAssessmentRequest pupaTestAndCocoonAssessmentRequest){
+        ResponseWrapper rw = ResponseWrapper.createWrapper(LotGroupageResponse.class);
+
+        rw.setContent(cocoonMarketService.savePupaTestAndCocoonAssessmentResult(pupaTestAndCocoonAssessmentRequest));
+        return ResponseEntity.ok(rw);
+
     }
 
     @PostMapping("/getPupaTestAndCocoonAssessmentResult")
