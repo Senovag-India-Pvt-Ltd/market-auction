5dec276de836c0fb6a16d2c448e8461c54d251e7
Shraddha <shraddhanku2001@gmail.com>
2026-04-20 13:31:46 +0530

    Changes

--- src/main/java/com/sericulture/marketandauction/repository/LotGroupageRepository.java
+++ src/main/java/com/sericulture/marketandauction/repository/LotGroupageRepository.java
@@ -2,8 +2,11 @@ package com.sericulture.marketandauction.repository;
 
 import com.sericulture.marketandauction.model.entity.LotGroupage;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+
+import jakarta.persistence.QueryHint;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
@@ -22,6 +25,7 @@ public interface LotGroupageRepository extends PagingAndSortingRepository<LotGro
 
     Optional<LotGroupage> findByLotGroupageIdAndActiveIn(long lotGroupageId, Set<Boolean> active);
 
+    @QueryHints(@QueryHint(name = "org.hibernate.flushMode", value = "COMMIT"))
     @Query(nativeQuery = true, value = "SELECT ma.market_auction_id,l.lot_id " +
             "FROM market_auction ma " +
             "LEFT JOIN lot l ON ma.market_auction_id = l.market_auction_id " +
@@ -1106,5 +1110,23 @@ public interface LotGroupageRepository extends PagingAndSortingRepository<LotGro
             @Param("marketId") Integer marketId
     );
 
+    @Query(nativeQuery = true, value = """
+            SELECT evba.virtual_account_number, rvcb.CURRENT_BALANCE
+            FROM dbo.external_unit_registration eur
+            INNER JOIN dbo.external_unit_type_master et ON et.external_unit_type_id = eur.external_unit_type_id
+            LEFT JOIN dbo.eu_virtual_bank_account evba ON evba.eu_id = eur.external_unit_registration_id AND evba.market_master_id = :marketId
+            LEFT JOIN dbo.REELER_VID_CURRENT_BALANCE rvcb ON rvcb.reeler_virtual_account_number = evba.virtual_account_number
+            WHERE eur.external_unit_registration_id = :externalUnitId AND et.payment_via_bank = 1
+            """)
+    Object[][] getExternalUnitVirtualAccountBalance(@Param("externalUnitId") Long externalUnitId, @Param("marketId") int marketId);
 
+    @Query(nativeQuery = true, value = """
+            SELECT evba.virtual_account_number, rvcb.CURRENT_BALANCE
+            FROM dbo.external_unit_registration eur
+            INNER JOIN dbo.external_unit_type_master et ON et.external_unit_type_id = eur.external_unit_type_id
+            LEFT JOIN dbo.eu_virtual_bank_account evba ON evba.eu_id = eur.external_unit_registration_id AND evba.market_master_id = :marketId
+            LEFT JOIN dbo.REELER_VID_CURRENT_BALANCE rvcb ON rvcb.reeler_virtual_account_number = evba.virtual_account_number
+            WHERE eur.external_unit_registration_id = :externalUnitId AND et.payment_via_bank = 1
+            """)
+    Object[] getExternalUnitVirtualAccountBalanceInSave(@Param("externalUnitId") Long externalUnitId, @Param("marketId") int marketId);
 }
